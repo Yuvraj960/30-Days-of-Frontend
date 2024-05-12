050bcd771e2aa6ba7dddc0a88e2a53efd6ddd796
Yuvraj960 <yuvrajgupta956@gmail.com>
2024-05-12 23:26:29 +0530

    Modified file names for better sorting

--- Day 4 - 3d Navigation/3d.js
+++
@@ -1,9 +0,0 @@
-const hamburger_menu = document.querySelector('.hamburger'),
-    container = document.querySelector('.container');
-
-
-
-    hamburger_menu.addEventListener("click", ()=>
-    {
-        container.classList.toggle("active");
-    })
